a4020d595cc08971d1dea16f2a33a70862f7bbf2
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2020-10-21 11:26:01 -0600

    Add P2P node

---
+++ start-node-p2p.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "No network or P2P ports provided"
+    exit 1
+fi
+
+cd ~/eth2/$1/node/
+NAME=$1-node-0-node
+
+if ! podman ps | grep $NAME ; then
+    if podman ps -a | grep $NAME ; then
+        podman rm $NAME
+    fi
+    podman run \
+        --name $NAME \
+        -v ./data/:/data:Z \
+        -p 0.0.0.0:$2:12000 \
+        -p 0.0.0.0:$3:13000 \
+        --net=$1 \
+        --rm -d gcr.io/prysmaticlabs/prysm/beacon-chain:latest --accept-terms-of-use --datadir=/data --rpc-host=0.0.0.0 --grpc-gateway-host=0.0.0.0 --monitoring-host=0.0.0.0 --http-web3provider=https://goerli.prylabs.net --$1 --p2p-host-ip=$(curl -s v4.ident.me)
+fi
+podman logs -f $1-node-0-node
+

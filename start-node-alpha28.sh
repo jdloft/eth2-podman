00c6c4ee0739b5543ddef7c2d2e1ba4e3d660734
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2020-10-24 14:10:27 -0600

    Add alpha 28 node

---
+++ start-node-alpha28.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "No network provided"
+    exit 1
+fi
+
+cd eth2/$1/node/
+NAME=$1-node-0-node
+
+if ! podman ps | grep $NAME ; then
+    if podman ps -a | grep $NAME ; then
+        podman rm $NAME
+    fi
+    podman run \
+        --name $NAME \
+        -v ./data/:/data:Z \
+        --net=$1 \
+        --rm -d gcr.io/prysmaticlabs/prysm/beacon-chain:alpha28 --datadir=/data --rpc-host=0.0.0.0 --grpc-gateway-host=0.0.0.0 --monitoring-host=0.0.0.0 --http-web3provider=https://goerli.prylabs.net --$1 --min-sync-peers 0
+fi
+podman logs -f $1-node-0-node
+
